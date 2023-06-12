ead7526380e0d3cb17135047eb36a9215009fee1
helmi0611 <110041171+helmi0611@users.noreply.github.com>
2023-06-12 22:44:42 +0700

    sec

--- src/components/Vehicle.jsx
+++ src/components/Vehicle.jsx
@@ -3,11 +3,6 @@ import './Vehicle.css';
 import { useState } from 'react';
 
 const Vehicle = ({ carName, carImage, carDesk, carMerk }) => {
-  const [liked, setLiked] = useState(false);
-  const toggleLike = () => {
-    setLiked(!liked);
-  
-  };
 
   const [showDescription, setShowDescription] = useState(false);
   const toggleDescription = () => {
@@ -15,7 +10,7 @@ const Vehicle = ({ carName, carImage, carDesk, carMerk }) => {
   };
 
   const [like,setLike] = useState(true);
-  function likeChanger (){
+  function toogleLiked (){
     setLike(!like);
 }
 
@@ -26,8 +21,16 @@ const Vehicle = ({ carName, carImage, carDesk, carMerk }) => {
         <h2>{carName}</h2>
 
         <img style={{ width: 250, height: 150, borderRadius: 8, }} src={carImage} alt={carName} />
-        <button style={{background :like ? 'blue':'red', padding:'5px', margin:'5px', borderRadius:'8px'}}onClick={()=>likeChanger()}>{like? "! Suka":"Batal Suka"}</button>        
-        <button style={{padding:'5px', margin:'5px', borderRadius:'8px'}} onClick={toggleDescription}>
+        <button style={{
+          background :like ? 'blue':'red',
+          padding:'5px', 
+          margin:'5px', 
+          borderRadius:'8px'}}onClick={()=>toogleLiked()}>{like? "! Suka":"Batal Suka"}</button>
+
+        <button style={{
+          padding:'5px', 
+          margin:'5px', 
+          borderRadius:'8px'}} onClick={toggleDescription}>
         {showDescription ? 'Sembunyikan Deskripsi' : 'Selengkapnya'}
         </button>
 
